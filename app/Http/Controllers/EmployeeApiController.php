c395d0da4b8d509551a2608833b16a36c163e70e
issa <issa@adminb2b.local>
2020-02-06 17:44:30 +0100

    crud employee

---
+++ app/Http/Controllers/EmployeeApiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      return Employee::paginate(10);  //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $e= Employee::create($request->all());
+        return $e; 
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Employee $employee)
+    {
+       return $employee;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Employee $employee)
+    {
+        //return null;//  
+        return $employee->update($request->all());  //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Employee $employee)
+    {
+        $e =$employee;
+        $employee->delete();
+        return $e;
+    }
+
+/**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'birth_date' => ['required', 'date'],
+            'hire_date' => ['required', 'date'],
+            'gender'    => ['required', 'string', 'max:1', 'min:1', 'regex:[MF]'],
+        ]);
+    }
+} 
